59d9ee5d0aa2b630728b4d4d29fb8ccf20ba987c
kmitch453 <79026320+kmitch453@users.noreply.github.com>
2021-04-25 14:58:53 -0500

    Update table.php

--- table.php
+++ table.php
@@ -1,1 +1,16 @@
 
+<?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+  //table function
+    var $i, $j;
+    echo "<table style="width:100%">";
+    for($i=0; $i<=10; $i++) {
+      echo "<tr>";
+      for($j=0; $j<=10; $j++) {
+        echo "<td>" . $i*$j . "</td>";
+      }
+      echo "</tr>";
+    }
+    echo "</table>";
+  ?>
