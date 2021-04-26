d6cd21fa810db14cebfcec148d1f02037ac0221b
kmitch453 <79026320+kmitch453@users.noreply.github.com>
2021-04-26 18:00:34 -0500

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,17 +1,16 @@
 <html>
 <body>
 <?php
-//$grade = $_POST["color"]+$_POST["IronMan"]+$_POST["element"]+$_POST["video"]+$_POST["class"];
-echo "Which color in the light spectrum has the shortest wavelength?<br>
-  Your Answer: " . $_POST["color"] . "Correct Answer: purple<br>"
-echo "Who plays Iron Man in the Marvel Cinematic Universe?<br>
-  Your Answer: " . $_POST["IronMan"] . "Correct Answer: Robert Downey Jr.<br>"
-echo "What is not an elemental nation in Avatar: The Last Airbender?<br>
-  Your Answer: " . $_POST["element"] . "Correct Answer: light<br>"
-echo "What videogame is Tracer from?<br>
-  Your Answer: " . $_POST["video"] . "Correct Answer: Overwatch<br>"
-echo "What class is this project for?<br>
-  Your Answer: " . $_POST["class"] . "Correct Answer: EECS 448<br>"
+$answer1 = $_POST['color'];
+$answer2 = $_POST['IronMan'];
+$answer3 = $_POST['element'];
+$answer4 = $_POST['video'];
+$answer5 = $_POST['class'];
+echo "Which color in the light spectrum has the shortest wavelength? <br>Your Answer: $answer1 <br>Correct Answer: purple<br><br>";
+echo "Who plays Iron Man in the Marvel Cinematic Universe? <br>Your Answer: $answer2 <br>Correct Answer: Robert Downey Jr.<br><br>";
+echo "What is not an elemental nation in Avatar: The Last Airbender? <br>Your Answer: $answer3 <br>Correct Answer: light<br><br>";
+echo "What videogame is Tracer from? <br>Your Answer: $answer4 <br>Correct Answer: Overwatch<br><br>";
+echo "What class is this project for? <br>Your Answer: $answer5 <br>Correct Answer: EECS 448<br><br>";
 ?>
 </body>
 </html>
