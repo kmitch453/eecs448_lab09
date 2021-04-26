93afda0ede35f07ee63b5160546f92e95ee11db9
kmitch453 <79026320+kmitch453@users.noreply.github.com>
2021-04-25 23:55:41 -0500

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,4 +1,17 @@
 <html>
 <body>
 <?php
-  
+//$grade = $_POST["color"]+$_POST["IronMan"]+$_POST["element"]+$_POST["video"]+$_POST["class"];
+echo "Which color in the light spectrum has the shortest wavelength?<br>
+  Your Answer: " . $_POST["color"] . "Correct Answer: purple<br>"
+echo "Who plays Iron Man in the Marvel Cinematic Universe?<br>
+  Your Answer: " . $_POST["IronMan"] . "Correct Answer: Robert Downey Jr.<br>"
+echo "What is not an elemental nation in Avatar: The Last Airbender?<br>
+  Your Answer: " . $_POST["element"] . "Correct Answer: light<br>"
+echo "What videogame is Tracer from?<br>
+  Your Answer: " . $_POST["video"] . "Correct Answer: Overwatch<br>"
+echo "What class is this project for?<br>
+  Your Answer: " . $_POST["class"] . "Correct Answer: EECS 448<br>"
+?>
+</body>
+</html>
